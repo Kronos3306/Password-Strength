ae8f55d8932ef06f468c4590ada60a266c61cabf
Kronos3306 <casual.usn.3306@gmail.com>
2022-11-22 15:51:57 -0600

    Initial Commit

---
+++ Password Strength/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Password Strength
+//
+//  Created by Gleb Bair on 11/22/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
